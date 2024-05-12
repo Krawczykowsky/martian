7f9a2dc98cb32616755f2e48e6f3d52c6d081a92
Krawczykowsky <krawczykowski.maciej@gmail.com>
2024-05-12 14:44:30 +0200

    mevement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,24 @@
 extends CharacterBody2D
 
-
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
-	velocity.y += 400 * delta
-	move_and_slide()
+@export var gravity = 400
+@export var speed = 120
+@export var jump_force = 200
 
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+	
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jump_force
+	
+	
+	var direction = Input.get_axis("move_left", "move_right")	
+	velocity.x = direction * speed	
+	move_and_slide()
 
 
 
