e576151592473aca45e3dd6ac7b39d10f91c7bf1
Krawczykowsky <krawczykowski.maciej@gmail.com>
2024-05-13 22:20:07 +0200

    movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,7 +7,7 @@ extends CharacterBody2D
 @export var jump_force = 200
 
 func _physics_process(delta):
-	if is_on_floor() == false:
+	if not is_on_floor():
 		velocity.y += gravity * delta
 		if velocity.y > 500:
 			velocity.y = 500
@@ -16,38 +16,23 @@ func _physics_process(delta):
 		velocity.y = -jump_force
 	
 	
-	var direction = Input.get_axis("move_left", "move_right")	
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
+		
 	velocity.x = direction * speed	
 	move_and_slide()
+	update_animation(direction)
 
+func update_animation(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else: 
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
 
-
-
-
-
-
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-#
-## Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-#
-#
-#func _physics_process(delta):
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
